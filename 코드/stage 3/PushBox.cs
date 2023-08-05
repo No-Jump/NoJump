ea00daf3b2e83953af27c146d136093c0a50019c
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2023-08-05 17:48:15 +0900

    Update PushBox.cs

--- 코드/stage 3/PushBox.cs
+++ 코드/stage 3/PushBox.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class PushBox : MonoBehaviour
 {
     Rigidbody2D rbody;
-    //PhysicsMaterial2D material;
     Player player;
 
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
-        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
     }
 
@@ -22,12 +20,10 @@ public class PushBox : MonoBehaviour
             if (player.level == 0)
             {
                 rbody.mass = 100;
-                //material.friction = 100;
             }
             else
             {
                 rbody.mass = 10;
-                //material.friction = 5;
             }
         }
     }
