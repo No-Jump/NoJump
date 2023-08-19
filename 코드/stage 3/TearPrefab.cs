f3257ab174d8ffeb44b45df7d72975fcc4d24abe
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2023-08-19 16:01:40 +0900

    Update TearPrefab.cs

--- 코드/stage 3/TearPrefab.cs
+++ 코드/stage 3/TearPrefab.cs
@@ -6,15 +6,23 @@ public class TearPrefab : MonoBehaviour
 {
     public int damage = 1;
 
+    GlassBottle bottle;
     Player player;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        bottle = GameObject.FindWithTag("GlassBottle").GetComponent<GlassBottle>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.tag == "GlassBottle")
+        {
+            bottle.DamageToBottle(damage);
+            Destroy(this.gameObject);
+        }
+
         if (other.gameObject.tag == "Player")
         {
             player.TakeDamage(damage);
